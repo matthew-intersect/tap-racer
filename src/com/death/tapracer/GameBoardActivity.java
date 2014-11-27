31e3a27dd46ba51b0c3353c3ae0e4bf5a27af536
matthew-intersect <matthew@intersect.org.au>
2014-11-27 15:27:58 +1100

    Add basic timer functionality

--- src/com/death/tapracer/GameBoardActivity.java
+++ src/com/death/tapracer/GameBoardActivity.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -14,6 +15,7 @@ import android.widget.GridLayout.LayoutParams;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GameBoardActivity extends Activity {
 	
@@ -30,6 +32,7 @@ public class GameBoardActivity extends Activity {
 	ImageView currentTile;
 	
 	int score = 0;
+	TextView scoreBox;
 	
 	@SuppressLint("NewApi")
 	@Override
@@ -41,10 +44,12 @@ public class GameBoardActivity extends Activity {
 		
 		setContentView(R.layout.activity_game_board);
 		
+		scoreBox = (TextView) findViewById(R.id.game_score);
+		
 		tiles = new ArrayList<ImageView>();
 		populateGameBoard();
 		
-		changeTile(tiles.get(new Random().nextInt(9)));
+		startGame();
 	}
 
 	private void populateGameBoard() {
@@ -57,6 +62,24 @@ public class GameBoardActivity extends Activity {
 			row3.addView(generateTile());
 		}
 	
+	}
+	
+	private void startGame() {
+		final TextView timer = (TextView) findViewById(R.id.game_timer);
+		timer.setText("30");
+		new CountDownTimer(30000, 1000) {
+
+		     public void onTick(long millisUntilFinished) {
+		         timer.setText(String.valueOf(millisUntilFinished / 1000));
+		     }
+
+		     public void onFinish() {
+		    	 timer.setText("0");
+		    	 System.out.println(score);
+		     }
+		  }.start();
+		
+		changeTile(tiles.get(new Random().nextInt(9)));
 	}
 	
 	private ImageView generateTile() {
@@ -84,11 +107,14 @@ public class GameBoardActivity extends Activity {
 	private void processClick(ImageView tile) {
 		if (tile == currentTile) {
 			score++;
+			scoreBox.setText(String.valueOf(score));
 			ImageView newTile = tiles.get(new Random().nextInt(9));
 			while (newTile == currentTile) {
 				newTile = tiles.get(new Random().nextInt(9));
 			}
 			changeTile(newTile);
+		} else {
+			//TODO: lose game
 		}
 	}
 	
